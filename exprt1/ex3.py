92316630672af3bce8c42df4ba38a2160a267588
NikolaiK_qa <68595787+NikolaiKovalchuk@users.noreply.github.com>
2022-05-29 11:53:36 +0200

    Add files via upload

---
+++ exprt1/ex3.py
@@ -0,0 +1,13 @@
+a = input ("Введите дату: ")
+b = input ("Введите задачу: ")
+a2 = input ("Введите дату: ")
+b2 = input ("Введите задачу: ")
+a3 = input ("Введите дату: ")
+b3 = input ("Введите задачу: ")
+dictionary = {
+    a: b,
+    a2: b2,
+    a3: b3,    
+}
+
+print(dictionary)
